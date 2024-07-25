f5ac171026985af38cd756e5bb3118665be4974a
elbayoumi <mohamedashrafelbayoumi@gmail.com>
2024-07-25 16:32:09 +0300

    add_build

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var socketsRouter = require('./routes/sockets');
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,7 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', socketsRouter);
+app.use('/', indexRouter);
+app.use('/sockets', socketsRouter);
+
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const { exec } = require('child_process');
+const path = require('path');
+const targetDirectory = path.join(__dirname, '../');
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('index', { title: 'Expresss' });
 });
+
+
+router.get('/git_pull', (req, res) => {
+  // console.log(`Source Path: ${targetDirectory}`);
+  const command = 'git pull'; // Replace with your command
+  // res.json(targetDirectory)
+
+  exec(command, { cwd: targetDirectory }, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error}`);
+      return res.status(500).json({ data: '', message: `Error: ${error.message}`, error: true });
+    }
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      return res.status(500).json({ data: '', message: `stderr: ${stderr}`, error: true });
+    }
+    console.log(`stdout: ${stdout}`);
+    res.json({ data: stdout, message: 'Command executed successfully', error: false });
+  });
+});
+
 
 module.exports = router;

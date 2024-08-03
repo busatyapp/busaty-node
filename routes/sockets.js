b946afb5d2b54169c9df89d07674d10cf7f620d3
elbayoumi <mohamedashrafelbayoumi@gmail.com>
2024-08-03 20:26:34 +0300

    Added import for handleMzaodinSocket in sockets/index.js

--- routes/sockets.js
+++ routes/sockets.js
@@ -4,5 +4,10 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.send('Sockets endpoint');
 });
+app.post('/use', (req, res) => {
+  const { eventName, message } = req.body;
+  io.sockets.emit(eventName, message);
+  res.send('Message sent');
+});
 
 module.exports = router;

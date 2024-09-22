a65c65dce711ca3a55082f80c2e1b8a02d691985
elbayoumi <mohamedashrafelbayoumi@gmail.com>
2024-09-22 13:37:42 +0300

    Updated StaffTableSeed.php to modify static messages array with additional messages and id fields.

--- sockets/adminsSocket.js
+++ sockets/adminsSocket.js
@@ -1,6 +1,19 @@
 const handleAdminsSocket = (io, socket) => {
-    socket.on("admins", (eventName, message) => {
-        io.sockets.emit(eventName, message);
+    socket.on("admins", (message) => {
+        const schema = Joi.object().keys({
+            bus_id: Joi.string().required(),
+            // Add more fields to validate as needed
+          });
+      
+          const { error, value } = schema.validate(message);
+      
+          if (error) {
+            console.error(`Invalid message: ${error.message}`);
+            return;
+          }
+      
+          const eventName = value.bus_id;
+          io.sockets.emit(eventName, message);
     });
 };
 
--- sockets/attendantSocket.js
+++ sockets/attendantSocket.js
@@ -1,7 +1,21 @@
-const handleِAttendantsSocket = (io, socket) => {
-    socket.on("attendants", (eventName, message) => {
-        io.sockets.emit(eventName, message);
-    });
-};
+const Joi = require('joi');
 
+const handleِAttendantsSocket = (io, socket) => {
+  socket.on('attendants', (message) => {
+    const schema = Joi.object().keys({
+      bus_id: Joi.string().required(),
+      // Add more fields to validate as needed
+    });
+
+    const { error, value } = schema.validate(message);
+
+    if (error) {
+      console.error(`Invalid message: ${error.message}`);
+      return;
+    }
+
+    const eventName = value.bus_id;
+    io.sockets.emit(eventName, message);
+  });
+};
 module.exports = handleِAttendantsSocket;

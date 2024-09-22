87ae7344668b19509e6a5a70e23eaf6fba1fa897
elbayoumi <mohamedashrafelbayoumi@gmail.com>
2024-09-22 13:48:22 +0300

    Updated StaffTableSeed.php to modify static messages array with additional messages and id fields.

--- sockets/adminsSocket.js
+++ sockets/adminsSocket.js
@@ -1,19 +1,7 @@
 const handleAdminsSocket = (io, socket) => {
     socket.on("admins", (message) => {
-        const schema = Joi.object().keys({
-            bus_id: Joi.string().required(),
-            // Add more fields to validate as needed
-          });
-      
-          const { error, value } = schema.validate(message);
-      
-          if (error) {
-            console.error(`Invalid message: ${error.message}`);
-            return;
-          }
-      
-          const eventName = value.bus_id;
-          io.sockets.emit(eventName, message);
+        const eventName = JSON.parse(message).bus_id;
+        io.sockets.emit(eventName, message);
     });
 };
 
